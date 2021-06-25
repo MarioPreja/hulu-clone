d8338c474a28f85818cb72a542900b773e4f0714
marjuspreja <marjus.preja.ext@neveling.net>
2021-06-25 10:34:12 +0200

    fixes

--- components/Results.js
+++ components/Results.js
@@ -2,10 +2,9 @@ import Thumbnail from './Thumbnail'
 import FlipMove from 'react-flip-move'
 
 function Results({results}) {
-  console.log(results + ' result.js');
     return (
       <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex'>
-        {results.map(result => (
+        {results?.map(result => (
           <Thumbnail key={result.id} result={result}/>
           ))}
       </FlipMove>
